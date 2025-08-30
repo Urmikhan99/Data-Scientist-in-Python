ce5df067cde7db072aa75c14cda4843bbf561b59
Urmi khan <157728763+Urmikhan99@users.noreply.github.com>
2025-08-30 22:07:47 +0600

    Variables & Types

---
+++ python/Variables_and_Types.py
@@ -0,0 +1,76 @@
+height =1.79
+weight =68.7
+bmi =weight/height **2
+
+print(height)
+
+# BMI=height2weight​ 
+print(68.7/1.79**2)
+
+print(bmi)
+print(type(bmi))
+'''
+# Note 
+# In Python, ** means “power” (exponent).
+'''
+
+# python type
+day_of_week = 5
+print(type(day_of_week))
+
+# python types 2
+x="body mass index"
+y="this works too"
+print(type(y))
+
+z=True
+print(type(z))
+
+# python type 3
+print(2+3)
+
+print ('ab'+'cd')
+# Strings can be joined: 'ab' + 'cd' = 'abcd'
+
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print (savings)
+
+
+'''
+Create a variable monthly_savings, equal to 10 and num_months, equal to 4.
+Multiply monthly_savings by num_months and assign it to new_savings.
+Print the value of new_savings.
+'''
+
+# Create the variables monthly_savings and num_months
+monthly_savings =10
+num_months = 4
+
+# Multiply monthly_savings and num_months
+new_savings = 10*4
+
+# Print new_savings
+print(new_savings)
+
+
+'''
+Create a new float, half, with the value 0.5.
+Create a new string, intro, with the value "Hello! How are you?".
+Create a new boolean, is_good, with the value True.
+'''
+# Create a variable half
+half=0.5
+
+# Create a variable intro
+intro="Hello! How are you?"
+
+# Create a variable is_good
+is_good =True
+
+print(half)
+print(intro)
+print(is_good)
